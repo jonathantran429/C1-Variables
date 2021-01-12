bcc037e1c5efb088e858565ac2a9aed6a8c1c5ec
Jonathan  Tran <110579@eesd.org>
2021-01-12 00:31:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#Jonathan Tran
+#Variables are containers, and they hold a value
+#Data Types/Variable Types:
+#String (str) : combination of characters (letters, numbers, or symbols, must use quotation marks)
+#Integer (int) : whole numbers
+#Characters (Char) : single letter, number, or symbol, use apostrophes
+#Float : numbers with decimals
+#Boolean (boo) : True/False statements
+
+#formula:
+#variableName = value
+#Rules for variable names
+#1. always start with a letter
+#2. can only contain numbers and letters
+#3. if more than one word is used, use camelCase (first word is lowercase and every word after that is uppercase)
+
+firstName = "Jonathan" #String type
+
+favoriteBook = "None"
+favNumber = 2
+isHungry = True
+favChar = 'j'
+pi = 3.14159
